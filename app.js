10078d6fea02d7866675b81cde6937630dfc1d98
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-06-22 12:39:04 +0530

    Add homepage

---
+++ app.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //Toggle nav  
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 1s ease forwards ${index / 7 + 0.8}s`;
+            }
+        });
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
